06c7612e9528db243ed81447ae4900c3ca2e2e4c
stormappears <92795413+stormappears@users.noreply.github.com>
2022-01-26 01:28:43 +0530

    Add files via upload

---
+++ productData.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        id    :  '1',
+        name  :  'Photoshop',
+        price :  '300'
+    },
+    {
+        id    :  '2',
+        name  :  'Webstorm',
+        price :  '200'
+    },
+    {
+        id    :  '3',
+        name  :  'Figma',
+        price :  '20'
+    },
+    {
+        id    :  '4',
+        name  :  'Raju',
+        price :  '20'
+    },
+    {
+        id    :  '5',
+        name  :  'KAju',
+        price :  '20'
+    }
+
+
+]
+
+    module.exports = products;
